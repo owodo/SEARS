f224c6bab6daa043442480252c75742ec6af1177
arinmashta <60249729+arinmashta@users.noreply.github.com>
2026-04-27 12:41:44 -0400

    Change redirect URL in signUp function

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -113,7 +113,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   };
 
   const signUp = async (email: string, password: string, firstName: string, lastName: string) => {
-    const redirectUrl = `${window.location.origin}/`;
+    const redirectUrl = `${window.location.origin}/auth/callback`;
     
     const { error } = await supabase.auth.signUp({
       email,
